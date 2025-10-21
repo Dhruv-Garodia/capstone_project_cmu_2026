c3fe223bdf994563f83c630e728b8d936108ce3a
niconicoo0w0 <zhilanw2@illinois.edu>
2025-10-20 22:15:10 -0400

    .

--- pfib_puma/make_synthetic_meth.sh
+++ pfib_puma/make_synthetic_meth.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-SPEC_FILE=${1:-data/config/log_normal_auto.txt}
-PNG_DIR=${2:-output/lognormal/png_slices}
-OUT_MESH=${3:-pfib_mesh_auto_200bin.stl}
+SPEC_FILE=${1:-data/config/adjusted_config.txt}
+PNG_DIR=${2:-output/synthetic_pfibs/synthetic_10/png_slices}
+OUT_MESH=${3:-synthetic_10.stl}
 
 python script/generate_synthetic_pfib.py --spec "$SPEC_FILE"
+cp "$SPEC_FILE" "$(dirname "$PNG_DIR")/"
 python script/convert_mesh.py --png_dir "$PNG_DIR" --out_mesh "$OUT_MESH"
