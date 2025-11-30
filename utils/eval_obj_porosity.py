07988383c0a2b5319d63841be5e9bd07e50edbe0
niconicoo0w0 <zhilanw2@gmail.com>
2025-11-29 21:46:06 -0500

    add recon test result json

--- utils/eval_obj_porosity.py
+++ utils/eval_obj_porosity.py
@@ -1,18 +1,20 @@
 import trimesh
+import os
 
-mesh = trimesh.load("data/model-recon-output/synthetic_5/voxel_direct.obj")
-solid_volume = mesh.volume
-bbox_volume = mesh.bounding_box.volume
-pore_volume = bbox_volume - solid_volume
+base = "data/model-recon-output"
 
-porosity = pore_volume / bbox_volume
-print(porosity)
+for i in range(0, 11):
+    path = os.path.join(base, f"synthetic_{i}", "voxel_direct.obj")
+    
+    print(f"\n=== synthetic_{i} ===")
+    try:
+        mesh = trimesh.load(path)
+        solid_volume = mesh.volume
+        bbox_volume = mesh.bounding_box.volume
+        pore_volume = bbox_volume - solid_volume
 
-# evalated on voxel_direct.obj
-# (pred, gt)
-#0 0.3516312222222222 0.425199
-#1 0.39724590123456793 0.471001
-#2 0.43498564197530865 0.506801
-#3 0.4768557901234568 0.54663
-#4 0.5254917530864198 0.593496
-#5 0.5664025679012346 0.630187
+        porosity = pore_volume / bbox_volume
+        print("porosity:", porosity)
+
+    except Exception as e:
+        print("Error loading mesh:", e)
